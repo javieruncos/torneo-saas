9dbd1b883ad6ada9754479a4d742e2dfea17e22d
Javier Uncos <javiersrfc30@gmail.com>
2026-04-09 17:27:31 -0300

    servicio para crear clubes

--- src/app/api/teams/route.ts
+++ src/app/api/teams/route.ts
@@ -1,21 +1,44 @@
 import connectDB from "@/app/lib/db/db";
 import { createTeam, getTeams } from "@/app/lib/services/team.service";
+import { fileURLToPath } from "node:url";
 
 export const POST = async (req: Request) => {
-    await connectDB();
-    const formData = await req.formData();
-    
-    const name = formData.get("name");
-    const file = formData.get("file");
-    
+  await connectDB();
+  const formData = await req.formData();
 
-    const data = await req.json();
-    const team = await createTeam(data);
-    return Response.json(team);
+  console.log({
+    name: formData.get("name"),
+    category: formData.get("category"),
+    shortname: formData.get("shortname"),
+  });
+
+  //extrar los datos del form
+  const data = {
+    name: formData.get("name") as string,
+    shortname: formData.get("shortname") as string,
+    city: formData.get("city") as string,
+      category: formData.get("category") as string, // 🔥 ESTE FALTABA
+    active: formData.get("active") === "on" ? true : false,
+    description: formData.get("description") as string,
+   colors: formData.get("colors")
+  };
+
+  const file = formData.get("file") as File;
+
+  if (!data.name || !data.shortname || !data.city) {
+    return new Response("Faltan datos", { status: 400 });
+  }
+
+  try {
+    const club = await createTeam(data, file);
+    return Response.json(club);
+  } catch (error: any) {
+    return Response.json({ error: error.message }, { status: 500 });
+  }
 };
 
 export const GET = async (req: Request) => {
-    await connectDB();
-    const teams = await getTeams();
-    return Response.json(teams);
+  await connectDB();
+  const teams = await getTeams();
+  return Response.json(teams);
 };
--- src/app/lib/services/team.service.ts
+++ src/app/lib/services/team.service.ts
@@ -1,21 +1,82 @@
 import { Team } from "@/app/types/team";
 import Clubes from "@/app/lib/models/teams.model";
+import cloudinary from "../cloudinary";
 
-export const createTeam = async (data: Team) => {
-    try {
-        const response = await Clubes.create(data);
-        return response;
-    } catch (error) {
-        console.log(error);
-        return error;  
+export const createTeam = async (data: any, file: File) => {
+    console.log("DATA EN SERVICE:", data);
+  try {
+    if (!file) {
+      throw new Error("la imagen es requerida");
     }
+
+    const { name, shortname, category, description, city, active, colors } =
+      data;
+
+    const exists = await Clubes.findOne({ name: data.name });
+
+    if (exists) {
+      throw new Error("El club ya existe");
+    }
+
+    let parsedColors = {};
+    if (colors) {
+      try {
+        parsedColors = typeof colors === "string" ? JSON.parse(colors) : colors;
+      } catch (error) {
+        return Response.json({ error: "Invalid colors" }, { status: 400 });
+      }
+    }
+
+    const baseSlug = name
+      .toLowerCase()
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "")
+      .replace(/[^a-z0-9]+/g, "-")
+      .replace(/(^-|-$)+/g, "");
+
+    const uniqueSlug = `${baseSlug}-${Date.now()}`;
+
+    //convertir la imagen a buffer
+    const bytes = await file.arrayBuffer();
+    const buffer = Buffer.from(bytes);
+
+    const upload = await new Promise<any>((resolve, reject) => {
+      const stream = cloudinary.uploader.upload_stream(
+        { folder: "clubes", public_id: uniqueSlug },
+        (error, result) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(result);
+          }
+        },
+      );
+
+      stream.end(buffer);
+    });
+
+    const club = await Clubes.create({
+      ...data,
+      colors: parsedColors,
+      slug: uniqueSlug,
+      logo: {
+        url: upload.secure_url,
+        public_id: upload.public_id,
+      },
+    });
+
+    return club;
+
+  } catch (error: any) {
+    throw new Error(error.message || "Error al crear el equipo");
+  }
 };
 
 export const getTeams = async () => {
-    try {
-        const res = await fetch("/api/teams");
-        return res.json();
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    const res = await fetch("/api/teams");
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
 };

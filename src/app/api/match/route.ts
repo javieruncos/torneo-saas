4c041f430f2efeae36215323512a89236940cbd1
Javier Uncos <javiersrfc30@gmail.com>
2026-04-22 17:12:40 -0300

    funciones Crud de match

--- src/app/api/match/route.ts
+++ src/app/api/match/route.ts
@@ -1,22 +1,44 @@
 import connectDB from "@/app/lib/db/db";
-import { createMatch } from "@/app/lib/service/macth.service";
-
+import Match from "@/app/lib/models/match.models";
+import { createMatch, getMatches } from "@/app/lib/service/macth.service";
+import mongoose from "mongoose";
 
 export const POST = async (req: Request) => {
-    await connectDB();
-    
-    const data = await req.json();
+  await connectDB();
 
-    if (!data.homeTeam || !data.awayTeam || !data.date || !data.time || !data.fieldNumber) {
-        return Response.json({ error: "Faltan datos" }, { status: 400 });
-    }
+  const data = await req.json();
 
-    try {
-        const match = await createMatch(data);
-        return Response.json(match, { status: 201 });
-    } catch (error: any) {
-        return Response.json({ error: error.message || "Error al crear el partido" }, { status: 400 });
-        
-    }
-    
-}
+  if (
+    !data.homeTeam ||
+    !data.awayTeam ||
+    !data.date ||
+    !data.time ||
+    !data.fieldNumber
+  ) {
+    return Response.json({ error: "Faltan datos" }, { status: 400 });
+  }
+
+  try {
+    const match = await createMatch(data);
+    return Response.json(match, { status: 201 });
+  } catch (error: any) {
+    return Response.json(
+      { error: error.message || "Error al crear el partido" },
+      { status: 400 },
+    );
+  }
+};
+
+export const GET = async (req: Request) => {
+  await connectDB();
+  try {
+    const matches = await getMatches()
+    return Response.json(matches, { status: 200 }); ;
+  } catch (error: any) {
+    return Response.json({ error: error.message }, { status: 500 });
+  }
+};
+
+
+
+
--- src/app/lib/service/macth.service.ts
+++ src/app/lib/service/macth.service.ts
@@ -1,17 +1,19 @@
+import mongoose from "mongoose";
 import Match from "../models/match.models";
 
 type data = {
   homeTeam: string;
   awayTeam: string;
-  date: Date;
-  time: string;
-  fieldNumber: number;
+  date?: Date;
+  time?: string;
+  fieldNumber?: number;
+  status?:"pending"|"finished";
 };
 
 export const createMatch = async (data: data) => {
   const { homeTeam, awayTeam, date, time, fieldNumber } = data;
 
-  if (!homeTeam || !awayTeam || !date || !time || !fieldNumber) {
+  if (!homeTeam || !awayTeam || !date || !time || fieldNumber === undefined) {
     throw new Error("Faltan datos");
   }
 
@@ -31,6 +33,83 @@ export const createMatch = async (data: data) => {
 
     return match;
   } catch (error: any) {
-    throw new Error(error.message), { status: 500 };
+    throw new Error(error.message);
+  }
+};
+
+
+export const getMatches = async () => {
+  try {
+    const matchs = await Match.find().populate(
+      "homeTeam awayTeam",
+      "name shortname logo",
+    );
+
+    if (!matchs) {
+      throw new Error("Partidos no encontrados");
+    }
+
+    return matchs;
+  } catch (error) {
+    throw new Error("Error al obtener los partidos");
+  }
+};
+
+export const getMatchById = async (id: string) => {
+  try {
+    const matchs = await Match.findById(id);
+    if (!matchs) {
+      throw new Error("Partido no encontrado");
+    }
+    return matchs;
+  } catch (error) {
+    throw new Error("Error al obtener el partido");
+  }
+};
+
+export const updateMatch = async (id: string, data: data) => {
+ 
+  if(!mongoose.Types.ObjectId.isValid(id)) {
+    throw new Error("ID inválido");
+  }
+
+  try {
+    const match = await Match.findByIdAndUpdate(id, data, { new: true });
+
+    if (!match) {
+      throw new Error("Partido no encontrado");
+    }
+
+    if (match.status === "finished") {
+      throw new Error("El partido ya ha finalizado");
+    }
+
+    if (match.homeTeam === match.awayTeam) {
+      throw new Error("Los equipos deben ser diferentes");
+    }
+
+    if (data.date !== undefined) match.date = data.date;
+    if (data.time !== undefined) match.time = data.time;
+    if (data.fieldNumber !== undefined) match.fieldNumber = data.fieldNumber;
+    if (data.status) match.status = data.status;
+
+    await match.save();
+
+    return match;
+  } catch (error) {
+    throw new Error("Error al actualizar el partido");
+  }
+};
+
+
+export const deleteMatch = async (id: string) => {
+  try {
+    const match = await Match.findByIdAndDelete(id);
+    if (!match) {
+      throw new Error("Partido no encontrado");
+    }
+    return match;
+  } catch (error) {
+    throw new Error("Error al eliminar el partido");
   }
 };
